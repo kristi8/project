ed4a7b23528b53b49d415949f328dff631b9ece1
kristi8 <qristina.chelidze@gmail.com>
2022-11-25 00:33:11 +0400

    tyity

---
+++ css/script.js
@@ -0,0 +1,6 @@
+let navigation = document.getElementById('navbar');
+let burgerbar = document.getElementById('burgerBar');
+
+burgerbar.addEventListener('click', function(){
+   navigation.classList.add('activenav')
+})
